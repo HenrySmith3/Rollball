d084a98e89bee0035db56bd03444749bc9fdd34c
HSmith3 <henry.smith.athena@gmail.com>
2015-07-20 23:38:33 -0400

    Played around a little bit, nothing great.

---
+++ Assets/Scenes/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject following;
+	public int radius;
+	public float rotation;
+	public float sensitivity;
+	// Use this for initialization
+	void Start () {
+		rotation = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotation += Input.GetAxis ("Mouse X") * sensitivity;
+		Vector3 offset = new Vector3(Mathf.Sin(rotation)*radius,-20,Mathf.Cos(rotation)*radius);
+		transform.position = following.transform.position - offset;
+
+		transform.LookAt (following.transform.position);
+	}
+}
---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		bool jump = Input.GetKeyDown ("space");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, jump ? 20:0, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+}
